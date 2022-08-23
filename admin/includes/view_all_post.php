b126d40d689d82fea2ba92da32db4b410dc921b2
tiansing <christian_igbaras@yahoo.com>
2022-08-23 21:40:03 +0800

    Revert "clone function added"
    This reverts commit 70312dfc80d0e0d52a744b088b152ef4872fba51.

--- admin/includes/view_all_post.php
+++ admin/includes/view_all_post.php
@@ -24,30 +24,6 @@ if (isset($_POST['checkBoxArray'])) {
                 $publish_post_query = mysqli_query($connection, $query);
 
                 break;
-            case "clone":
-
-                $query = "SELECT * FROM posts WHERE post_id= {$postValueId}";
-                $select_posts_query = mysqli_query($connection, $query);
-
-                while ($row = mysqli_fetch_array($select_posts_query)) {
-
-                    $post_author = $row['post_author'];
-                    $post_title = $row['post_title'];
-                    $post_category_id = $row['post_category_id'];
-                    $post_status = $row['post_status'];
-                    $post_image = $row['post_image'];
-                    $post_tags = $row['post_tags'];
-                    $post_content = $row['post_content'];
-                }
-
-                $query = "INSERT INTO posts (post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
-                $query .= "VALUES({$post_category_id}, '{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_status}')";
-                $clone_post_query = mysqli_query($connection, $query);
-                if (!$clone_post_query) {
-                    die("CONNECTION FAILED " . mysqli_error($connection));
-                }
-
-                break;
         }
     }
 }
@@ -65,7 +41,6 @@ if (isset($_POST['checkBoxArray'])) {
                 <option value="published">Publish</option>
                 <option value="draft">Draft</option>
                 <option value="delete">Delete</option>
-                <option value="clone">Clone</option>
             </select>
         </div>
         <div class="form-group">
--- author.php
+++ author.php
@@ -9,57 +9,57 @@
 
     <div class="row">
 
-        <!`-- Blog Entries Column -->
-            <div class="col-md-8">
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
 
-                <?php
-                if (isset($_GET['author'])) {
-                    $author_filter = $_GET['author'];
-                }
-                $query = "SELECT * FROM posts WHERE post_author = '{$author_filter}'";
-                $select_author_query = mysqli_query($connection, $query);
-                if (!$select_author_query) {
-                    die("CONNECTION FAILED" . mysqli_error($connection));
-                }
-                while ($row = mysqli_fetch_assoc($select_author_query)) {
-                    $post_title = $row["post_title"];
-                    $post_author = $row["post_author"];
-                    $post_date = $row["post_date"];
-                    $post_image = $row["post_image"];
-                    $post_content = $row["post_content"];
+            <?php
+            if (isset($_GET['author'])) {
+                $author_filter = $_GET['author'];
+            }
+            $query = "SELECT * FROM posts WHERE post_author = '{$author_filter}'";
+            $select_author_query = mysqli_query($connection, $query);
+            if (!$select_author_query) {
+                die("CONNECTION FAILED" . mysqli_error($connection));
+            }
+            while ($row = mysqli_fetch_assoc($select_author_query)) {
+                $post_title = $row["post_title"];
+                $post_author = $row["post_author"];
+                $post_date = $row["post_date"];
+                $post_image = $row["post_image"];
+                $post_content = $row["post_content"];
 
-                ?>
+            ?>
 
-                    <h1 class="page-header">
-                        Page Heading Author
-                        <small>Secondary Text</small>
-                    </h1>
+                <h1 class="page-header">
+                    Page Heading Author
+                    <small>Secondary Text</small>
+                </h1>
 
-                    <!-- First Blog Post -->
-                    <h2>
-                        <a href="#"><?php echo $post_title ?></a>
-                    </h2>
-                    <p class="lead">
-                        Author <?php echo $post_author ?>
-                    </p>
-                    <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
-                    <hr>
-                    <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
-                    <hr>
-                    <p><?php echo $post_content ?></p>
-
-
-                    <hr>
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                <hr>
+                <p><?php echo $post_content ?></p>
 
 
-                <?php  } ?>
+                <hr>
+
+
+            <?php  } ?>
 
 
 
-            </div>
+        </div>
 
-            <!-- Blog Sidebar Widgets Column -->
-            <?php include "includes/sidebar.php" ?>
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include "includes/sidebar.php" ?>
 
 
     </div>
